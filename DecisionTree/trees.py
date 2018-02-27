7957b5c0de32c7b26d3b047c2407bd24f04627ac
wengyan <wengyan123@gmail.com>
2018-02-27 12:34:37 +0800

    starting decision tree

---
+++ DecisionTree/trees.py
@@ -0,0 +1,67 @@
+from math import log
+import _operator
+
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:   # the number of unique elements and their occurance
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)  # log base 2
+    return shannonEnt
+
+
+def splitDataset(dataSet, axis, value):
+    retDataSet =[]
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]  # chop out axis used for splitting
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1  # the last column is used for the labels
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):  # iterate over all the features
+        featList = [example[i] for example in dataSet]  # create a list of all the examples of this feature
+        uniqueVals = set(featList)  # get a set of unique values
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataset(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy  # calculate the info gain; ie reduction in entropy
+        if infoGain > bestInfoGain:   # compare this to the best gain so for
+            bestInfoGain = infoGain   # if better than current best, set to best
+            bestFeature = i
+    return bestFeature
+
+
+def main():
+    dataSet, labels = createDataSet()
+    shannonEnt = calcShannonEnt(dataSet)
+    print(shannonEnt)
+
+
+if __name__ == '__main__':
+    main()
