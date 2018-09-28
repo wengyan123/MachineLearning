d682788a0f8563b2107c810a98e26918055133ff
wengyan <yan.weng@outlook.com>
2018-09-29 07:33:39 +0800

    SVM init

---
+++ SVM/svmMLiA.py
@@ -0,0 +1,42 @@
+import numpy as np
+from time import sleep
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
+
+
+def selectJrand(i, m):
+    j = i
+    while (j==i):
+        j = int(np.random.uniform(0, m))
+    return j
+
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = np.mat(dataMatIn)
+    labelMat = np.mat(classLabels).transpose()
+    b = 0
+    m, n = np.shape(dataMatrix)
+    alphas = np.mat(np.zeros((m, 1)))
+    iter = 0
+    while (iter < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(np.mulitply(alphas,labelMat).T * (dataMatrix*dataMatrix[i,:].T)) + b
+            Ei = fXi - float(labelMat[i])   # checks if an example violates KKT conditions
